18e3249822fb091ca2e9b6ca92ff43f9c4b7ee0c
一块DanceSystem <656344827@qq.com>
2021-11-05 20:42:38 +0800

    modify bug 2

--- GAME/urls/index.py
+++ GAME/urls/index.py
@@ -6,8 +6,4 @@ urlpatterns = [
         path("menu/", include("GAME.urls.menu.index")),
         path("playground/",include("GAME.urls.playground.index")),
         path("settings/",include("GAME.urls.settings.index")),
-<<<<<<< HEAD
         ]
-=======
-]
->>>>>>> 30b51107dc5a042037bc39adc01ab5721e24d030
